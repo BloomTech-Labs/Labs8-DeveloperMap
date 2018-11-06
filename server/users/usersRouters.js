f0c322ce00dc6d98e29ac5ce18fedb254a541e9d
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-06 15:55:23 -0800

    routers for search for all users + by uid

--- server/users/usersRouters.js
+++ server/users/usersRouters.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   rootRef
+    .child('users')
     .once('value')
     .then(snapshot => {
       res.json(snapshot);
@@ -13,6 +14,21 @@ router.get('/', (req, res) => {
       res.json(err);
     });
 });
+
+router.get('/:uid', (req, res) => {
+  const { uid } = req.params;
+  rootRef
+    .child('users/' + uid)
+    .once('value')
+    .then(snapshot => {
+      res.json(snapshot);
+    });
+});
+
+// router.post('/', (req, res) => {
+//   const {email, phoneNumber, location, employer, uid}
+//   rootRef.child('users/')
+// })
 
 router.get('/', (req, res) => {});
 
