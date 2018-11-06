6034e45055788a5cc022b9bd6cc992d8ae90d9fa
Austinlp4 <apendergrast44@gmail.com>
2018-11-06 16:09:56 -0600

    Added Routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -6,9 +7,18 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Developer Map</h1>
+        <Route exact path='/' component={App}/>
+        <Route path='/employer/:employerId' component={EmployerProfile}/>
+        <Route path='/seeker/:seekerId' component={SeekerProfile}/>
+        <Route path='/seeker/:seekerId/settings' component={SeekerSettings}/>
+        <Route path='/employer/:employerId/settings' component={EmployerSettings}/>
+        <Route path='/employer/:employerId/settings/billing' component={EmployerBilling}/>
+        <Route path='/seeker/:seekerId/favorites' component={SeekerFavorites}/>
+        <Route path='/signin' component={Signin}/>
+        <Route path='/Signup' component={Signup}/>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
