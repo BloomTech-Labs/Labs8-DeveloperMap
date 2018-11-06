1724796e6b84bfa40296d8c96e7ad2b7ed998b73
Austinlp4 <apendergrast44@gmail.com>
2018-11-06 17:24:19 -0600

    Add components and fix react-router
    Added component templates for the routes and built on the structure of the app. Fixed the routes and moved the node_modules to the inside of the client.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import { Route, NavLink, withRouter } from 'react-router-dom';
 import './App.css';
+import EmployerBilling from './components/Employer/BillingModal/EmployerBilling';
+import EmployerSettings from './components/Employer/SettingEmployerModal/EmployerSettings';
+import EmployerProfile from './components/EmployerProfile/EmployerProfile';
+import SeekerFavorites from './components/Seeker/SeekerFavorites';
+import SeekerSettings from './components/Seeker/SeekerSettings';
+import SeekerProfile from './components/SeekerProfile/SeekerProfile';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Developer Map</h1>
-        <Route exact path='/' component={App}/>
+        
         <Route path='/employer/:employerId' component={EmployerProfile}/>
         <Route path='/seeker/:seekerId' component={SeekerProfile}/>
         <Route path='/seeker/:seekerId/settings' component={SeekerSettings}/>
         <Route path='/employer/:employerId/settings' component={EmployerSettings}/>
         <Route path='/employer/:employerId/settings/billing' component={EmployerBilling}/>
         <Route path='/seeker/:seekerId/favorites' component={SeekerFavorites}/>
-        <Route path='/signin' component={Signin}/>
-        <Route path='/Signup' component={Signup}/>
+        <Route path='/signin' component={SignIn}/>
+        <Route path='/Signup' component={SignUp}/>
       </div>
     );
   }
---
+++ client/src/components/Employer/BillingModal/EmployerBilling.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class EmployerBilling extends React.Component {
+    render() {
+        return <div></div>
+    }
+}
+
+export default EmployerBilling;
