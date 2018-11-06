67cf936811c893cad687646ebc58324da89947af
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-06 13:28:38 -0800

    error handle

--- server/routers/firebaseRouters.js
+++ server/routers/firebaseRouters.js
@@ -4,9 +4,14 @@ const rootRef = firebase.database().ref();
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  rootRef.once('value').then(snapshot => {
-    res.json(snapshot);
-  });
+  rootRef
+    .once('value')
+    .then(snapshot => {
+      res.json(snapshot);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
