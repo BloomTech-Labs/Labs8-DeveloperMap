a3c85d1ad88318bbbfc63f1d853723734a434a00
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-07 14:02:29 -0800

    commented out old routes + error handling added to job post change router

--- server/users/usersRouters.js
+++ server/users/usersRouters.js
@@ -5,74 +5,99 @@ const router = express.Router();
 
 //const updateData
 
-router.get('/', (req, res) => {
-  rootRef
-    .child('seekers')
-    .once('value')
-    .then(snapshot => {
-      res.json(snapshot);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+// router.get('/', (req, res) => {
+//   rootRef
+//     .child('seekers')
+//     .once('value')
+//     .then(snapshot => {
+//       res.json(snapshot);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
-router.get('/:uid', (req, res) => {
-  const { uid } = req.params;
-  rootRef
-    .child('users/' + uid)
-    .once('value')
-    .then(snapshot => {
-      res.json(snapshot);
-    });
-});
+// router.get('/:uid', (req, res) => {
+//   const { uid } = req.params;
+//   rootRef
+//     .child('users/' + uid)
+//     .once('value')
+//     .then(snapshot => {
+//       res.json(snapshot);
+//     });
+// });
 
-//Change UserInfo Status
+// //Change UserInfo Status
 
-router.put('/userInfo/:name/:uid', (req, res) => {
-  const { uid, name } = req.params;
-  console.log(name);
-  if (name === 'location' || name === 'employer' || name === 'email') {
-    const newDate = req.body;
-    const updateObject = {
-      [`users/${uid}/${name}`]: newDate[name],
-    };
-    rootRef
-      .update(updateObject)
-      .then(() => {
-        res.json(`${name} status updated`);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  } else {
-    res.json({ err: 'Must send location, email, employer' });
-  }
-});
+// router.put('/userInfo/:name/:uid', (req, res) => {
+//   const { uid, name } = req.params;
+//   console.log(name);
+//   if (name === 'location' || name === 'employer' || name === 'email') {
+//     const newDate = req.body;
+//     const updateObject = {
+//       [`users/${uid}/${name}`]: newDate[name],
+//     };
+//     rootRef
+//       .update(updateObject)
+//       .then(() => {
+//         res.json(`${name} status updated`);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   } else {
+//     res.json({ err: 'Must send location, email, employer' });
+//   }
+// });
 
 //Change data for all posts under a company's uid
 
 router.put('/jobs/:uid/:jb', (req, res) => {
   const { uid, jb } = req.params;
-  const newData = req.body;
-  let updateObject = {};
-  updateObject[`companyPostings/${uid}/${jb}`] = newData;
+  const { companyName, date, location, jobLink, jobTitle } = req.body;
 
-  rootRef
-    .child(`favoriteLookup/${uid}`)
-    .once('value')
-    .then(snapshot => {
-      const lookups = Object.keys(snapshot.val());
-      lookups.forEach(lookup => {
-        updateObject[`favoritePostings/${lookup}/${jb}`] = newData;
-      });
-    })
-    .then(() => {
-      rootRef.update(updateObject).then(() => {
-        console.log(updateObject);
-        res.json(rootRef);
-      });
+  //Checks object format
+  if (!companyName || !date || !location || !jobLink || !jobTitle) {
+    res.json({
+      err:
+        'Missing keys. Must have the following keys: companyName, date, location, jobLink, and jobTitle',
     });
+  } else {
+    const newData = { companyName, date, location, jobLink, jobTitle };
+
+    let updateObject = {};
+    updateObject[`companyPostings/${uid}/${jb}`] = newData;
+
+    rootRef
+      .child(`favoriteLookup/${uid}`)
+      .once('value')
+      .then(snapshot => {
+        const lookups = Object.keys(snapshot.val());
+
+        lookups.forEach(lookup => {
+          updateObject[`favoritePostings/${lookup}/${jb}`] = newData;
+        });
+      })
+      .catch(err =>
+        res.json({
+          err: `did not have permission to access rootRef/favoriteLookup/${uid}.`,
+        })
+      )
+      .then(() => {
+        rootRef.update(updateObject).then(() => {
+          res.json(rootRef);
+        });
+      })
+      .catch(err =>
+        res.json({
+          err:
+            'did not have permissions to access one of the following references: ' +
+            Object.keys(updateObject)
+              .join(', ')
+              .trim(),
+        })
+      );
+  }
 });
 
 //add Links
