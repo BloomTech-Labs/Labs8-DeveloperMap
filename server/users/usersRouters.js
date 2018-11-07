4604f2ad891dfc7c40045740229dc1ea7430aab8
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-06 16:36:06 -0800

    updates for location, employer, email

--- server/users/usersRouters.js
+++ server/users/usersRouters.js
@@ -25,6 +25,60 @@ router.get('/:uid', (req, res) => {
     });
 });
 
+//Change Email
+
+router.put('/email/:uid', (req, res) => {
+  const { uid } = req.params;
+  const { email } = req.body;
+  const updateObject = {
+    [`users/${uid}/email`]: email,
+  };
+  rootRef
+    .update(updateObject)
+    .then(() => {
+      res.json('email updated');
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//Change Location
+
+router.put('/location/:uid', (req, res) => {
+  const { uid } = req.params;
+  const { location } = req.body;
+  const updateObject = {
+    [`users/${uid}/location`]: location,
+  };
+  rootRef
+    .update(updateObject)
+    .then(() => {
+      res.json('location updated');
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//Change Employer Status
+
+router.put('/employer/:uid', (req, res) => {
+  const { uid } = req.params;
+  const { employer } = req.body;
+  const updateObject = {
+    [`users/${uid}/employer`]: employer,
+  };
+  rootRef
+    .update(updateObject)
+    .then(() => {
+      res.json('employer status updated');
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 // router.post('/', (req, res) => {
 //   const {email, phoneNumber, location, employer, uid}
 //   rootRef.child('users/')
