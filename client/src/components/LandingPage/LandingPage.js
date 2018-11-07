69acf1ff4fb00e7a27c4cb5df65cdf3bb1aff8b0
lndubose <lndubose13@gmail.com>
2018-11-06 18:10:52 -0600

    Functional Map
    --Map is working on the landing page
    --Got rid of App.css, it was causing issues with the map being displayed wrong
    --Still need to work on the map for markers and styling but it is running

---
+++ client/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibG5kdWJvc2UiLCJhIjoiY2pvNmF1ZnowMGo3MDNrbmw4ZTVmb2txMyJ9.UpxjYyEOBnCJjw_qE_N8Kw';
+
+class LandingPage extends React.Component {
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v10',
+    });
+  }
+
+  componentWillUnmount() {
+    this.map.remove();
+  }
+
+  render() {
+    const style = {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      width: '100%',
+    };
+
+    return (
+      <div>
+        <div style={style} ref={el => (this.mapContainer = el)} />
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
