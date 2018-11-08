a4361ff5c9ed95b48be76660c316f538587e81de
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-11-08 13:20:02 -0700

    Set Up Server For Heroku Deployment
    - Reconfigure Package.json, and add Procfile.
    - Server must be its own git repo to deploy. No change here.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const server = require('./server');
-const port = 9000;
+require('dotenv').config();
+
+const port = process.env.PORT || 9000;
+
 server.listen(port, err => {
   if (err) {
     console.log(err);
