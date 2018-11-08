59cbc57211247bbcb1d6d043c352f211eee5af62
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-08 13:11:02 -0800

    Realtime Database hooked up to Component: SeekerProfile

--- client/src/components/SeekerProfile/SeekerProfile.js
+++ client/src/components/SeekerProfile/SeekerProfile.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import axios from 'axios';
+import firebase from '../../firebase/firebase';
 
 class SeekerProfile extends React.Component {
-    render() {
-        return <div></div>
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      bio: '',
+      email: '',
+      firstName: '',
+      github: '',
+      jobTitle: '',
+      lastName: '',
+      linkedIn: '',
+      location: '',
+      phoneNumber: '',
+      portfolio: '',
+      twitter: '',
+    };
+  }
+  render() {
+    return <div />;
+  }
+  componentDidMount() {
+    const uid = 'uid1';
+    firebase
+      .database()
+      .ref()
+      .child(`seekers/${uid}`)
+      .once('value')
+      .then(snapshot => {
+        this.setState(snapshot.val());
+      });
+  }
 }
 
 export default SeekerProfile;
