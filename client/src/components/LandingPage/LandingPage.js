37740db027b2645b937dfc4b89e7b8f04907997c
Austinlp4 <apendergrast44@gmail.com>
2018-11-08 08:55:39 -0600

    Make searchbar have styles so its not so hard to read
    I have added some styling to the search bar to give the suggestions list a background color as well as a pointer and hover color so it's easier to navigate through. I have also gotten rid of the annoying little button that had no purpose

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -8,24 +8,43 @@ import styled from 'styled-components';
 const MapWindow = styled.div`
    input{
      height: 30px;
-     width: 200px;  
+     width: 300px;  
      border-radius: 20px;
      border: none;
-     padding-left: 5px;
+     padding-left: 8px;
      &:focus {
        outline: none;
      }
+   }
+   .suggestions{
+     background-color: rgb(232,232,232);
+     border-radius: 4px;
+     padding-inline-start: 20px;
+     li{
+       list-style-type: none;
+       margin: 2px 0;
+       cursor: pointer;
+       &:hover{
+         color: rgb(54,154,255);
+       }
+     }
+     .active{
+      color: rgb(119,187,255);
+    }
    }
    .mapboxgl-control-container{
      width: 100%;
 
      .mapboxgl-ctrl-top-right{
-      margin-left: 5%;
+      margin-left: 3%;
       margin-top: 6px;
       right: initial;
       top: initial;
       left: initial;
     }
+   }
+   .geocoder-pin-right{
+     display: none;
    }
    
 `;
