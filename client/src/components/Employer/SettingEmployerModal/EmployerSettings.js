b6a3ae1813efd81627774b46a90645113a9c5372
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-08 13:40:12 -0800

    fixed pathing error with import firebase

--- client/src/components/Employer/SettingEmployerModal/EmployerSettings.js
+++ client/src/components/Employer/SettingEmployerModal/EmployerSettings.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import firebase from '../../firebase/firebase';
+import firebase from '../../../firebase/firebase';
 
 class EmployerSettings extends React.Component {
   constructor(props) {
