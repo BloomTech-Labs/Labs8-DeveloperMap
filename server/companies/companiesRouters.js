4c4431c7a3c91efc80674cb98c9ac84b4bc5c8ea
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-13 10:50:04 -0800

    POST added to companiesRouters

--- server/companies/companiesRouters.js
+++ server/companies/companiesRouters.js
@@ -27,3 +27,39 @@ router.get('/:uid', (req, res) => {
       res.status(500).json({ err });
     });
 });
+
+router.post('/addUser/:uid', (req, res) => {
+  const { uid } = req.params;
+  const {
+    companyName,
+    companyWebsite,
+    email,
+    location,
+    phoneNumber,
+  } = req.body;
+  const newData = {
+    companyName,
+    companyWebsite,
+    email,
+    location,
+    phoneNumber,
+  };
+  rootRef
+    .child(`companies/${uid}`)
+    .once('value')
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        res.status(500).json({ err: 'message already exists' });
+      } else {
+        snapshot.ref
+          .set(newData)
+          .then(() => {
+            res
+              .status(201)
+              .json({ message: `${email} has been added to database` });
+          })
+          .catch(err => res.json(err));
+      }
+    })
+    .catch(err => res.json(err));
+});
