0dadc0f5cac015790fd562ad4080ef102fe86f3a
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-13 10:51:47 -0800

    PUT added to companiesRouters.js

--- server/companies/companiesRouters.js
+++ server/companies/companiesRouters.js
@@ -63,3 +63,39 @@ router.post('/addUser/:uid', (req, res) => {
     })
     .catch(err => res.json(err));
 });
+
+router.put('/userInfo/companies/:uid', (req, res) => {
+  const { uid } = req.params;
+  const updateKeys = Object.keys(req.body);
+  rootRef
+    .child(`companies/${uid}`)
+    .once('value')
+    .then(snapshot => {
+      if (!snapshot.exists()) {
+        return res.json({ message: 'user does not exist' });
+      }
+      let updateObject = {};
+      snapshot.forEach(childSnap => {
+        const snapKey = childSnap.key;
+        if (updateKeys.includes(snapKey)) {
+          if (childSnap.val() === req.body[snapKey]) {
+            console.log(`${snapKey} data is same as posted data`);
+          } else {
+            updateObject[snapKey] = req.body[snapKey];
+          }
+        }
+      });
+      const updateNumber = Object.keys(updateObject).length;
+      if (updateNumber > 0) {
+        snapshot.ref
+          .update(updateObject)
+          .then(() => {
+            res.json(`userInfo updated in ${updateNumber} fields`);
+          })
+          .catch(err => res.json(err));
+      } else {
+        res.json('no new data was posted');
+      }
+    })
+    .catch(err => res.json(err));
+});
