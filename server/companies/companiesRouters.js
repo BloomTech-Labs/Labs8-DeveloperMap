2d638c387873d74aed2c0c56be284d9e60cd4eb6
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-13 10:47:25 -0800

    GETS added

--- server/companies/companiesRouters.js
+++ server/companies/companiesRouters.js
@@ -2,3 +2,28 @@ const express = require('express');
 const firebase = require('../firebase/firebase.js');
 const rootRef = firebase.database().ref();
 const router = express.Router();
+
+router.get('/', (req, res) => {
+  rootRef
+    .child('companies')
+    .once('value')
+    .then(snapshot => {
+      res.status(200).json(snapshot.val());
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
+});
+
+router.get('/:uid', (req, res) => {
+  const { uid } = req.params;
+  rootRef
+    .child(`companies/${uid}`)
+    .once('value')
+    .then(snapshot => {
+      res.status(200).json(snapshot.val());
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
+});
