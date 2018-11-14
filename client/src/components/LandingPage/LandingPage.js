6d3218793927b78e2e8b989eafbe6a78defa7b58
lndubose <lndubose13@gmail.com>
2018-11-14 12:43:10 -0600

    Render New Map
    -- Was able to get the new map to work with the react dependency that was added
    -- Got the styling to work with the map, back to how it was before
    -- Now going to get the geolocator working again

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,68 +1,51 @@
 import React from 'react';
-import "mapbox-gl/dist/mapbox-gl.css";
+import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
+import ReactMapGL from 'react-map-gl';
 import MapboxGeocoder from 'mapbox-gl-geocoder';
 import { MapWindow } from '../../styles/MapWindowStyle';
 
-
-
 const MAPBOX_TOKEN =
   'pk.eyJ1IjoibG5kdWJvc2UiLCJhIjoiY2pvNmF1ZnowMGo3MDNrbmw4ZTVmb2txMyJ9.UpxjYyEOBnCJjw_qE_N8Kw';
-mapboxgl.accessToken = MAPBOX_TOKEN;
 
 class LandingPage extends React.Component {
-  state={
+  state = {
     viewport: {
       width: 400,
       height: 400,
       latitude: 37.7577,
       longitude: -122.4376,
-      zoom: 8
+      zoom: 8,
     },
-    
-  }
+  };
 
-  componentDidMount() {
-    this.map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/lndubose/cjo6br6c61dfs2snxq0bxlg3f?optimize=true'
-    });
-    this.map.addControl(new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken
-  }));
-  }
+  // resize() {
+  //   this.onViewportChange({
+  //     width: window.innerWidth,
+  //     height: window.innerHeight
+  //   });
+  // }
 
-  componentWillUnmount() {
-    this.map.remove();
-  }
-  
-
-  resize() {
-    this.onViewportChange({
-      width: window.innerWidth,
-      height: window.innerHeight
-    });
-  }
-
-  // onViewportChange = viewport => {
-  //   this.setState({
-  //     viewport: { ...this.state.viewport, ...viewport }
-  //   })
-  // };
-  
   render() {
     const style = {
       position: 'absolute',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      width: '100%',
+      marginTop: '-8px',
+      marginLeft: '-8px',
     };
-    
-    return (    
+
+    return (
       <MapWindow>
-        <div style={style} {...this.state.viewport} ref={el => (this.mapContainer = el)} />
-      </MapWindow>        
+        <ReactMapGL
+          className="map"
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          {...this.state.viewport}
+          onViewportChange={viewport => this.setState({ viewport })}
+          style={style}
+          width="100%"
+          height="100%"
+          mapStyle="mapbox://styles/lndubose/cjo6br6c61dfs2snxq0bxlg3f?optimize=true"
+        />
+      </MapWindow>
     );
   }
 }
--- client/src/styles/MapWindowStyle.js
+++ client/src/styles/MapWindowStyle.js
@@ -1,45 +1,44 @@
 import styled from 'styled-components';
 
 export const MapWindow = styled.div`
-   input{
-     height: 30px;
-     width: 300px;  
-     border-radius: 20px;
-     border: none;
-     padding-left: 8px;
-     &:focus {
-       outline: none;
-     }
-   }
-   .suggestions{
-     background-color: rgb(232,232,232);
-     border-radius: 4px;
-     padding-inline-start: 20px;
-     li{
-       list-style-type: none;
-       margin: 2px 0;
-       cursor: pointer;
-       &:hover{
-         color: rgb(54,154,255);
-       }
-     }
-     .active{
-      color: rgb(119,187,255);
+  input {
+    height: 30px;
+    width: 300px;
+    border-radius: 20px;
+    border: none;
+    padding-left: 8px;
+    &:focus {
+      outline: none;
     }
-   }
-   .mapboxgl-control-container{
-     width: 100%;
+  }
+  .suggestions {
+    background-color: rgb(232, 232, 232);
+    border-radius: 4px;
+    padding-inline-start: 20px;
+    li {
+      list-style-type: none;
+      margin: 2px 0;
+      cursor: pointer;
+      &:hover {
+        color: rgb(54, 154, 255);
+      }
+    }
+    .active {
+      color: rgb(119, 187, 255);
+    }
+  }
+  .mapboxgl-control-container {
+    width: 100%;
 
-     .mapboxgl-ctrl-top-right{
+    .mapboxgl-ctrl-top-right {
       margin-left: 3%;
       margin-top: 6px;
       right: initial;
       top: initial;
       left: initial;
     }
-   }
-   .geocoder-pin-right{
-     display: none;
-   }
-   
+  }
+  .geocoder-pin-right {
+    display: none;
+  }
 `;
