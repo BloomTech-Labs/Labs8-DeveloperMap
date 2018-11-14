133fe2073c084e9a686856140697435ba1f6f8d1
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-14 10:38:21 -0800

    finished company auth middleware + error handling

--- server/auth/authMiddleware.js
+++ server/auth/authMiddleware.js
@@ -4,25 +4,89 @@ const firebase = require('../firebase/firebase.js');
 module.exports = { setSeekerClaims, verifySeekerToken };
 
 const setSeekerClaims = (req, res, next) => {
-  const { uid } = req.params;
-  firebase
-    .auth()
-    .setCustomUserClaims(uid, { seeker: true })
-    .then(() => {
-      next();
-    })
-    .catch(err => res.json(err));
+  if (req.headers.authorization) {
+    let idToken = req.headers.authorization;
+    firebase
+      .auth()
+      .verifyIdToken(idToken)
+      .then(decodedToken => {
+        const { uid } = decodedToken;
+        req.body.uid = uid;
+        firebase
+          .auth()
+          .setCustomUserClaims(uid, { seeker: true })
+          .then(() => {
+            next();
+          })
+          .catch(err => res.json(err));
+      })
+      .catch(err => res.json(err));
+  } else {
+    res.json({ err: 'Token was not received.' });
+  }
 };
 
 const verifySeekerToken = (req, res, next) => {
-  firebase
-    .auth()
-    .verifyIdToken(idToken)
-    .then(claims => {
-      if (claims.seeker) {
-        next();
-      } else {
-        res.json({ err: 'Does not have required role' });
-      }
-    });
+  if (req.headers.authorization) {
+    let idToken = req.headers.authorization;
+    firebase
+      .auth()
+      .verifyIdToken(idToken)
+      .then(decodedToken => {
+        if (decodedToken.seeker) {
+          const { uid } = decodedToken;
+          req.body.uid = uid;
+          next();
+        } else {
+          res.json({ err: 'Token does not have required claims' });
+        }
+      });
+  } else {
+    res.json({ err: 'Token was not received.' });
+  }
+};
+
+//----------------------------------------------------COMPANIES
+
+const setCompanyClaims = (req, res, next) => {
+  if (req.headers.authorization) {
+    let idToken = req.headers.authorization;
+    firebase
+      .auth()
+      .verifyIdToken(idToken)
+      .then(decodedToken => {
+        const { uid } = decodedToken;
+        req.body.uid = uid;
+        firebase
+          .auth()
+          .setCustomUserClaims(uid, { company: true })
+          .then(() => {
+            next();
+          })
+          .catch(err => res.json(err));
+      })
+      .catch(err => res.json(err));
+  } else {
+    res.json({ err: 'Token was not received.' });
+  }
+};
+
+const verifyCompanyToken = (req, res, next) => {
+  if (req.headers.authorization) {
+    let idToken = req.headers.authorization;
+    firebase
+      .auth()
+      .verifyIdToken(idToken)
+      .then(decodedToken => {
+        if (decodedToken.company) {
+          const { uid } = decodedToken;
+          req.body.uid = uid;
+          next();
+        } else {
+          res.json({ err: 'Token does not have required claims' });
+        }
+      });
+  } else {
+    res.json({ err: 'Token was not received.' });
+  }
 };
