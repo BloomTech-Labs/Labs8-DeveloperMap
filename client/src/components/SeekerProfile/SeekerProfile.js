ce999425a6b69893b2c8e5f99b955f578e4c55b7
Lndubose <lndubose13@gmail.com>
2018-11-20 15:39:59 -0600

    Intial Profile Modal

--- client/src/components/SeekerProfile/SeekerProfile.js
+++ client/src/components/SeekerProfile/SeekerProfile.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import firebase from '../../firebase/firebase';
+import { ModalContainer, ModalMain } from '../../styles/ModalGlobalStyle';
+
+import profile from '../../images/avatar-icon.jpg';
 
 class SeekerProfile extends React.Component {
   constructor(props) {
@@ -18,9 +21,7 @@ class SeekerProfile extends React.Component {
       twitter: '',
     };
   }
-  render() {
-    return <div />;
-  }
+
   componentDidMount() {
     const uid = 'uid1';
     firebase
@@ -31,6 +32,28 @@ class SeekerProfile extends React.Component {
       .then(snapshot => {
         this.setState(snapshot.val());
       });
+  }
+
+  render() {
+    return (
+      <ModalContainer>
+        <ModalMain>
+          <img src={profile} alt="" />
+          <h2>Name here</h2>
+          <h4>Location here</h4>
+          <p>Bio</p>
+          <div className="options">
+            <p>Open to remote work</p>
+          </div>
+          <div className="options">
+            <p>Open to relocation</p>
+          </div>
+          <div className="info">
+            <p>email</p>
+          </div>
+        </ModalMain>
+      </ModalContainer>
+    );
   }
 }
 
