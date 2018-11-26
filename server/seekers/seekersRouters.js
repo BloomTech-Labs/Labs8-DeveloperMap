c73ef343fccba7bd96f2eeee9966dc51f348e292
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-26 08:19:17 -0800

    seekers gets changed + get that uses uid from body added

--- server/seekers/seekersRouters.js
+++ server/seekers/seekersRouters.js
@@ -6,7 +6,7 @@ const authMw = require('../auth/authMiddleware.js');
 
 //----------------------------------------------------------------GETS
 
-router.get('/', (req, res) => {
+router.get('/all', (req, res) => {
   rootRef
     .child('seekers')
     .once('value')
@@ -20,6 +20,23 @@ router.get('/', (req, res) => {
 
 router.get('/:uid', (req, res) => {
   const { uid } = req.params;
+  rootRef
+    .child(`seekers/${uid}`)
+    .once('value')
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        res.status(200).json(snapshot.val());
+      } else {
+        res.status(404).json({ err: 'user not found' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
+});
+
+router.get('/', (req, res) => {
+  const { uid } = req.body;
   rootRef
     .child(`seekers/${uid}`)
     .once('value')
