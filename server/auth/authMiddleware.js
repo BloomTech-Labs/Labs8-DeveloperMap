aa1a213fbe6dcf24a148c11ac663784f459463ef
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-26 11:43:36 -0800

    console log added for testing

--- server/auth/authMiddleware.js
+++ server/auth/authMiddleware.js
@@ -20,6 +20,7 @@ const setSeekerClaims = (req, res, next) => {
               .auth()
               .createCustomToken(uid, claim);
             req.body.customToken = customToken;
+            console.log(req.body.customToken);
             next();
           })
           .catch(err => res.json(err));
