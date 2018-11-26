f15269e789411b845373c1a86c6e3b410ce16b8e
lndubose <lndubose13@gmail.com>
2018-11-26 16:58:46 -0600

    Add PUT and DELETE for favorite posting
    -- Put may need some adjustments based on how the job id is passed
    -- Delete will take the job off of the users favorites list

--- server/seekers/favoritesRoutes.js
+++ server/seekers/favoritesRoutes.js
@@ -21,4 +21,39 @@ router.get('/:uid', (req, res) => {
     });
 });
 
+router.put('/:uid', (req, res) => {
+  const { uid } = req.params;
+  const { jid } = req.body;
+  const newData = req.body;
+
+  let updateObject = {};
+  updateObject[`favoritePostings/${uid}/` + jid] = newData;
+
+  return rootRef
+    .update(updateObject)
+    .then(res => res.json('Job added to favorites.'))
+    .catch(err => res.status(500).json(`Server error --> ${err}`));
+});
+
+router.delete('/:uid/:jid', (req, res) => {
+  const { uid, jid } = req.params;
+
+  rootRef
+    .child(`favoritePostings/${uid}/${jid}`)
+    .once('value')
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        snapshot.ref
+          .remove()
+          .then(() => {
+            res.json('Job removed from favorites.');
+          })
+          .catch(() => res.json(err));
+      } else {
+        return res.json('Job not in favorites');
+      }
+    })
+    .catch(err => console.log(err));
+});
+
 module.exports = router;
