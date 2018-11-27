46200f04602ba5e946dc4521781b2384e028c87d
Austinlp4 <apendergrast44@gmail.com>
2018-11-26 18:16:52 -0600

    Add Styling to nav, conditional rendering, stripe fix

--- client/src/components/NavBar/NavBarView.js
+++ client/src/components/NavBar/NavBarView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Heart from '../../images/favorites-icon.png';
 import Avatar from '../../images/avatar-icon.jpg';
+import Logout from '../../images/logout.png';
 
 const Nav = styled.div`
   width: 98%;
@@ -21,79 +22,36 @@ const Nav = styled.div`
     }
   }
   .avatar {
-    margin-right: 2%;
+    
     img {
       border-radius: 50%;
       width: 27px;
       height: 27px;
     }
   }
-  .knobs {
-    display: flex;
-    width: 25%;
-    flex-direction: row;
-    justify-content: space-evenly;
-    align-items: center;
-    margin: 30%;
-  }
+  
 `;
 
-const ToggleKnob = styled.label`
-  position: relative;
-  width: 29px;
-  height: 12px;
-  justify-self: center;
-  align-items: center;
-  input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-    &:before {
-      position: absolute;
-      content: '';
-      height: 13px;
-      width: 13px;
-      left: 1px;
-      bottom: 0px;
-      background-color: white;
-      -webkit-transition: 0.4s;
-      transition: 0.4s;
-    }
-  }
-  input:checked + .slider {
-    background-color: #2196f3;
-  }
 
-  input:focus + .slider {
-    box-shadow: 0 0 1px #2196f3;
-  }
-
-  input:checked + .slider:before {
-    -webkit-transform: translateX(13px);
-    -ms-transform: translateX(13px);
-    transform: translateX(13px);
-  }
-  .slider.round {
-    border-radius: 12px;
-  }
-
-  .slider.round:before {
-    border-radius: 50%;
+const Icons = styled.div`
+  display: flex;
+  margin: 0 2%;
+  justify-content: space-between;
+  width: 100px;
+  .signout{
+    img {
+      border-radius: 50%;
+      width: 27px;
+      height: 27px;
+    } 
   }
 `;
 
 class NavBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
   handleSignIn = () => {
     this.props.history.push('/signin');
   };
@@ -102,30 +60,34 @@ class NavBar extends React.Component {
     this.props.history.push('/billing');
   }
 
+  profilePop = () => {
+    this.props.history.push(`/settings`);
+  }
+
 
   render() {
     return (
       <Nav>
-        <div className="knobs">
-          <ToggleKnob>
-            <input type="checkbox" />
-            <span className="slider round" />
-          </ToggleKnob>
-          <h5>Job Seeker</h5>
-          <ToggleKnob>
-            <input type="checkbox" />
-            <span className="slider round" />
-          </ToggleKnob>
-          <h5>Employer</h5>
-        </div>
+        {this.props.user ? (
+        <Icons>
         <div className="heart" onClick={this.billMe}>
           <img alt="Favorites Icon" src={Heart} />
         </div>
-        <div className="avatar" onClick={this.handleSignIn}>
-          <img alt="Avatar Icon" src={Avatar} />
+        <div className="signout">
+        <img alt='Logout' src={Logout} onClick={this.props.signOut}/>
         </div>
+        <div className="avatar">
+          <img alt="Avatar Icon" src={Avatar} onClick={this.profilePop}/>
+        </div>
+        </Icons>)
+        :
+        (
+          <div>
+            <button onClick={this.handleSignIn}>Sign In ></button>
+          </div>)}
       </Nav>
     );
+  
   }
 }
 
