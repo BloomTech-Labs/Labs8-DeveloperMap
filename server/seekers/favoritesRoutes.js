51584b55ef8637de4792b8f051bd162fe8cb98aa
lndubose <lndubose13@gmail.com>
2018-11-26 18:08:12 -0600

    Change PUT to POST, Add validate middleware, Add comments
    -- Changed the put to post to follow the guidelines of our backend
    -- The middleware makes sure that the data is present for the job posting being
    added

--- server/seekers/favoritesRoutes.js
+++ server/seekers/favoritesRoutes.js
@@ -3,6 +3,19 @@ const firebase = require('../firebase/firebase.js');
 const rootRef = firebase.database().ref();
 const router = express.Router();
 
+// ==== Middleware for favorites ====
+
+const validate = (req, res, next) => {
+  const { companyName, date, location, jobLink, jobTitle } = req.body;
+
+  if (!companyName || !date || !location || !jobLink || !jobTitle) {
+    res.status(400).json({ error: 'Data is missing for the job' });
+  } else {
+    next();
+  }
+};
+
+// ==== GET will return all the jobs favorited for the specific user ====
 router.get('/:uid', (req, res) => {
   const { uid } = req.params;
 
@@ -21,19 +34,30 @@ router.get('/:uid', (req, res) => {
     });
 });
 
-router.put('/:uid', (req, res) => {
-  const { uid } = req.params;
-  const { jid } = req.body;
+//  ==== POST will add the job favorited to the the list of favorites ====
+
+router.post('/:uid/:jid', validate, (req, res) => {
+  const { uid, jid } = req.params;
   const newData = req.body;
 
-  let updateObject = {};
-  updateObject[`favoritePostings/${uid}/` + jid] = newData;
+  rootRef
+    .once('value')
+    .then(snapshot => {
+      if (snapshot.child(`favoritePostings/${uid}/${jid}`).exists()) {
+        return res.status(400).json({ message: 'Job already in favorites' });
+      } else {
+        let updateObject = {};
+        updateObject[`favoritePostings/${uid}/${jid}`] = newData;
 
-  return rootRef
-    .update(updateObject)
-    .then(res => res.json('Job added to favorites.'))
-    .catch(err => res.status(500).json(`Server error --> ${err}`));
+        rootRef.update(updateObject);
+
+        res.status(200).json({ message: `Job has been added to favorites.` });
+      }
+    })
+    .catch(err => res.status(500).json({ error: `Server error --> ${err}` }));
 });
+
+// ==== DELETE will remove the job from the favorites list ====
 
 router.delete('/:uid/:jid', (req, res) => {
   const { uid, jid } = req.params;
@@ -46,11 +70,11 @@ router.delete('/:uid/:jid', (req, res) => {
         snapshot.ref
           .remove()
           .then(() => {
-            res.json('Job removed from favorites.');
+            res.json({ message: 'Job removed from favorites.' });
           })
           .catch(() => res.json(err));
       } else {
-        return res.json('Job not in favorites');
+        return res.json({ message: 'Job not in favorites' });
       }
     })
     .catch(err => console.log(err));
