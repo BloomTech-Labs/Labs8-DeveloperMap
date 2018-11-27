561f131bea470a3728c21ddaf4f14b92038cf9fa
Austinlp4 <apendergrast44@gmail.com>
2018-11-27 15:42:46 -0600

    New Api keys

--- client/src/components/Stripe/constants/stripe.js
+++ client/src/components/Stripe/constants/stripe.js
@@ -1,5 +1,5 @@
 const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
   ? 'pk_live_MY_PUBLISHABLE_KEY'
-  : 'pk_test_pDNaozIDLwRbPR2REq1yQ0aZ';
+  : 'pk_test_cU4wt2KwNDwC9R37YYXJC2NV';
 
 export default STRIPE_PUBLISHABLE;
