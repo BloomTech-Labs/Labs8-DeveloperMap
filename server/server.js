a0df67fb1f1c7b4950588f36a4d05b37a12834b8
Austinlp4 <apendergrast44@gmail.com>
2018-11-27 15:17:36 -0600

    Re-implement old cors

--- server/server.js
+++ server/server.js
@@ -7,21 +7,16 @@ const server = express();
 const seekersRouters = require('./seekers/seekersRouters.js');
 const companiesRouters = require('./companies/companiesRouters.js');
 const markersRouters = require('./markers/markersRouters.js');
-const CORS_WHITELIST = require('./constants/frontend');
+
 const favoritesRouters = require('./seekers/favoritesRoutes.js');
 
 // const configureServer = require('./serverConfig');
 const configureRoutes = require('./stripe-routes');
 
-server.use(express.json(), helmet());
+server.use(express.json(), helmet(), cors());
 //server.use('/api/database', usersRouters);
-const corsOptions = {
-  origin: (origin, callback) =>
-    (CORS_WHITELIST.indexOf(origin) !== -1 || !origin)
-      ? callback(null, true)
-      : callback(new Error('Not allowed by CORS'))
-};
-server.use(cors(corsOptions));
+
+
 server.use('/api/database/seekers', seekersRouters);
 server.use('/api/database/companies', companiesRouters);
 server.use('/api/markers', markersRouters);
