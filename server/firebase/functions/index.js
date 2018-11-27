6d149fc4ab58aa57476e24b4042bcc61d4f78555
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-27 09:49:11 -0800

    Added function that deletes all data related to user if removed from db

--- server/firebase/functions/index.js
+++ server/firebase/functions/index.js
@@ -6,7 +6,7 @@ const rootRef = firebase.database().ref();
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 //exports.labs8 = functions.https.onRequest(server);
-exports.deleteUser = functions.auth.user().onDelete(user => {
+exports.deleteUserAuth = functions.auth.user().onDelete(user => {
   const { uid } = user;
   console.log(user);
 
@@ -30,3 +30,55 @@ exports.deleteUser = functions.auth.user().onDelete(user => {
     })
     .catch(err => console.log(err));
 });
+
+exports.deleteUserDbSeeker = functions.database
+  .ref('/seekers/{uid}')
+  .onDelete((snapshot, context) => {
+    const uid = context.params.uid;
+    console.log(uid);
+    rootRef
+      .once('value')
+      .then(snapshot => {
+        let updateObject = {};
+        updateObject[`seekers/${uid}`] = null;
+        updateObject[`favoritePostings/${uid}`] = null;
+        updateObject[`companies/${uid}`] = null;
+        updateObject[`companyPostings/${uid}`] = null;
+        updateObject[`favoriteLookup/${uid}`] = null;
+        updateObject[`markers/${uid}`] = null;
+        snapshot.child('favoriteLookup').forEach(childSnap => {
+          if (childSnap.child(uid).exists()) {
+            updateObject[`favoriteLookup/${childSnap.key}/${uid}`] = null;
+          }
+        });
+        console.log('updateObject:', updateObject);
+        return rootRef.update(updateObject);
+      })
+      .catch(err => console.log(err));
+  });
+
+exports.deleteUserDbCompany = functions.database
+  .ref('/seekers/{uid}')
+  .onDelete((snapshot, context) => {
+    const uid = context.params.uid;
+    console.log(uid);
+    rootRef
+      .once('value')
+      .then(snapshot => {
+        let updateObject = {};
+        updateObject[`seekers/${uid}`] = null;
+        updateObject[`favoritePostings/${uid}`] = null;
+        updateObject[`companies/${uid}`] = null;
+        updateObject[`companyPostings/${uid}`] = null;
+        updateObject[`favoriteLookup/${uid}`] = null;
+        updateObject[`markers/${uid}`] = null;
+        snapshot.child('favoriteLookup').forEach(childSnap => {
+          if (childSnap.child(uid).exists()) {
+            updateObject[`favoriteLookup/${childSnap.key}/${uid}`] = null;
+          }
+        });
+        console.log('updateObject:', updateObject);
+        return rootRef.update(updateObject);
+      })
+      .catch(err => console.log(err));
+  });
