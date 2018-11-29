74268d4244b614c68c20645e6b01206b6fd0c47e
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-28 18:32:48 -0800

    jobPosting now changes to a based on a list

--- client/src/components/EmployerProfile/JobPosting.js
+++ client/src/components/EmployerProfile/JobPosting.js
@@ -4,28 +4,47 @@ import Heart from '../../images/hollow-heart.png';
 import FavHeart from '../../images/favorites-icon.png';
 
 class JobPosting extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            
-        }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    // Axios.get().then(response => {
+    //   const favoritedList = response.data;
+    //   if (favoritedList.includes(this.props.post.jobId)) {
+    //     document.querySelector(`#${jobId}`).setAttribute('src', FavHeart);
+    //   }
+    // });
+    const favoritedList = ['jb1'];
+    if (favoritedList.includes(this.props.post.jobId)) {
+      document
+        .querySelector(`#${this.props.post.jobId}`)
+        .setAttribute('src', FavHeart);
     }
-    render(){
-        return (
-            <Job>
-                <div>
-                <h2>{this.props.post.companyName}</h2>
-                <h3>{this.props.post.date}</h3>
-                <h4>{this.props.post.jobLink}</h4>
-                <h4>{this.props.post.jobTitle}</h4>
-                <h4>{this.props.post.location}</h4>
-                </div>
-                <div>                 
-                    <img alt='hollow heart' src={Heart} style={{width:'50px'}} onClick={this.props.favToggle}/>
-                </div>
-            </Job>
-        )
-    }
+  }
+
+  render() {
+    return (
+      <Job>
+        <div>
+          <h2>{this.props.post.companyName}</h2>
+          <h3>{this.props.post.date}</h3>
+          <h4>{this.props.post.jobLink}</h4>
+          <h4>{this.props.post.jobTitle}</h4>
+          <h4>{this.props.post.location}</h4>
+        </div>
+        <div>
+          <img
+            id={this.props.post.jobId}
+            alt="hollow heart"
+            src={Heart}
+            style={{ width: '50px' }}
+            onClick={this.props.favToggle}
+          />
+        </div>
+      </Job>
+    );
+  }
 }
 
 const Job = styled.div`
@@ -35,11 +54,11 @@ const Job = styled.div`
   height: 130px;
   max-width: 350px;
   width: 100%;
-  border: .7px solid rgba(220,220,220,.6);
+  border: 0.7px solid rgba(220, 220, 220, 0.6);
   padding: 5%;
   margin-bottom: 15px;
-  &:hover{
-      box-shadow: none;
+  &:hover {
+    box-shadow: none;
   }
 `;
 
