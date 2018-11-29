e10b6d34397780a3f717ae7f0282c1b65ebe21dd
lndubose <lndubose13@gmail.com>
2018-11-28 18:27:57 -0600

    Add icons to modal
    --Added the icons to the modal that are linkable to their own links they provide
    --If nothing provided, the icons will not appear
    --Starting to add responsiveness to this modal

---
+++ client/src/components/SeekerProfile/ProfileModalStyle.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+// Controls the content in the modal
+export const ProfileStyle = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+`;
+
+// Manipulates the icons on the side of the modal
+export const Icons = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  right: 0;
+  top: 15px;
+
+  img {
+    width: 60%;
+    margin: 3px 0;
+  }
+
+  .none {
+    display: none;
+  }
+`;
+
+// Controls Profile Picture
+export const ProfilePic = styled.img`
+  width: 25%;
+  border-radius: 50%;
+`;
+
+// Controls Name, Location, Bio
+export const MainContent = styled.div`
+  text-align: center;
+  width: 50%;
+
+  p {
+    padding: 5px 0;
+  }
+`;
+
+// Controls the checkboxes
+export const CheckLabel = styled.label`
+  display: block;
+  padding: 10px;
+`;
+
+// Controls the info box
+export const Info = styled.div`
+  border: 1px solid black;
+  padding: 5px 5%;
+  margin-bottom: 20px;
+`;
--- client/src/components/SeekerProfile/SeekerProfile.js
+++ client/src/components/SeekerProfile/SeekerProfile.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import axios from 'axios';
 import { ModalContainer, ModalMain } from '../../styles/ModalGlobalStyle.js';
-import { Info, ProfileStyle, CheckLabel } from './ProfileModal.js';
+import {
+  Info,
+  ProfileStyle,
+  CheckLabel,
+  ProfilePic,
+  Icons,
+  MainContent,
+} from './ProfileModalStyle.js';
 
+// Images for the Profile Modal
 import profile from '../../images/avatar-icon.jpg';
+import computerPic from '../../images/Icons/icons8-computer-monitor.png';
+import githubPic from '../../images/Icons/icons8-github.png';
+import linkedinPic from '../../images/Icons/icons8-linkedin-filled.png';
+import resumePic from '../../images/Icons/icons8-resume.png';
+import twitterPic from '../../images/Icons/icons8-twitter-filled.png';
 
 class SeekerProfile extends React.Component {
   constructor(props) {
@@ -16,12 +29,14 @@ class SeekerProfile extends React.Component {
       jobTitle: '',
       lastName: '',
       linkedIn: '',
-      location: '',
+      location: {},
       phoneNumber: '',
       portfolio: '',
-      twitter: '',
+      profilePicture: '',
       relocation: false,
       remote: false,
+      resume: '',
+      twitter: '',
     };
   }
 
@@ -34,7 +49,7 @@ class SeekerProfile extends React.Component {
       .then(response => this.setState({ ...response.data }))
       .catch(err => {
         console.log('%cNot a user', 'color: red', err);
-        this.props.history.push('/');
+        this.props.history.push('/nouser');
       });
   }
 
@@ -43,12 +58,58 @@ class SeekerProfile extends React.Component {
       <ModalContainer data-type="modal-container">
         <ModalMain>
           <ProfileStyle>
-            <img src={profile} alt="" />
-            <h2>{this.state.firstName}</h2>
-            <h4>
-              {this.state.location.city}, {this.state.location.state}
-            </h4>
-            {this.state.bio ? <p>Bio: {this.state.bio}</p> : <span />}
+            <Icons>
+              <a href={this.state.linkedIn} target="_blank">
+                <img
+                  className={this.state.linkedIn ? null : 'none'}
+                  src={linkedinPic}
+                  alt="LinkedIn Picture"
+                />
+              </a>
+              <a href={this.state.github} target="_blank">
+                <img
+                  className={this.state.github ? null : 'none'}
+                  src={githubPic}
+                  alt="Github Picture"
+                />
+              </a>
+              <a href={this.state.portfolio} target="_blank">
+                <img
+                  className={this.state.portfolio ? null : 'none'}
+                  src={computerPic}
+                  alt="Computer Picture"
+                />
+              </a>
+              <a href={this.state.twitter} target="_blank">
+                <img
+                  className={this.state.twitter ? null : 'none'}
+                  src={twitterPic}
+                  alt="Twitter Picture"
+                />
+              </a>
+              <a href={this.state.resume} target="_blank">
+                <img
+                  className={this.state.resume ? null : 'none'}
+                  src={resumePic}
+                  alt="Resume Picture"
+                />
+              </a>
+            </Icons>
+            {this.state.profilePicture ? (
+              <ProfilePic
+                src={this.state.profilePicture}
+                alt="profile picture"
+              />
+            ) : (
+              <ProfilePic src={profile} alt="default picture" />
+            )}
+            <MainContent>
+              <h2>{this.state.firstName}</h2>
+              <h4>
+                {this.state.location.city}, {this.state.location.state}
+              </h4>
+              {this.state.bio ? <p>Bio: {this.state.bio}</p> : <span />}
+            </MainContent>
             <div className="options">
               <CheckLabel>
                 <input
