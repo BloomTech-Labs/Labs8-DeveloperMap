5c72c9482e1fc3a2ae86ed98680c9def0cac0a44
Austinlp4 <apendergrast44@gmail.com>
2018-12-03 14:08:01 -0600

    Complete Nav guide for tutorial

---
+++ client/src/components/Tutorial/Navigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Navigating = () => {
+    return (
+        <NavGuide>
+            <h1>Navigation</h1>
+            <p>
+                You can use the search bar at the top left of the map to enter a location. The map will pan to the location 
+                on the map. 
+            </p>
+            <h1>Picture goes here</h1>
+            <p>By clicking and dragging the map you can also pan accross the map, if you are 
+                on mobile, you can zoom into a location by using two fingers and spreading them apart 
+                in any direction.
+            </p>
+            <h1>Picture goes here
+            </h1>
+        </NavGuide>
+    );
+}
+
+const NavGuide = styled.div`
+  padding: 5%;
+`;
+
+export default Navigating;
