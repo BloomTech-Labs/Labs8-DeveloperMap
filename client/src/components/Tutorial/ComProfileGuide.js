aab9257271b78574b0c74b46b4ee266162c8ce1f
Austinlp4 <apendergrast44@gmail.com>
2018-12-03 14:55:55 -0600

    company profile guide for tutorial

---
+++ client/src/components/Tutorial/ComProfileGuide.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import company from '../../images/markerlogo4.png';
+
+const ComProfileGuide = () => {
+    return (
+        <ProfileGuide>
+           <h1>Employer Profile</h1>
+           <p>
+               By clicking on a <span><img src={company} alt="" style={{width:'50px'}}/></span> marker 
+               on the map, you will be brought to that companies profile. You will see some general 
+               contact information for that company, as well as any jobs they have posted. By clicking the 
+               heart affiliated with each job.
+           </p>
+           <h1>Picture goes here</h1>
+           <p>To find jobs that have been favorited by you, click on the favorites icon on the Nav bar at 
+               the top of the screen.
+           </p>
+           <h1>Picture goes here</h1>
+           
+        </ProfileGuide>
+    );
+}
+
+const ProfileGuide = styled.div`
+  padding: 5%;
+`;
+
+export default ComProfileGuide;
