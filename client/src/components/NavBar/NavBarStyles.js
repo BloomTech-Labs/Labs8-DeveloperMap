24608f82def320dd4a1c52acc660600fe6347371
lndubose <lndubose13@gmail.com>
2018-12-04 12:53:57 -0600

    NavBar Responsive
    --The Navigation Bar is now responsive down to 315px
    --Learned how to change the width of the search bar for the geocoder

--- client/src/components/NavBar/NavBarStyles.js
+++ client/src/components/NavBar/NavBarStyles.js
@@ -1,17 +1,22 @@
 import styled from 'styled-components';
 
 export const Nav = styled.div`
-  width: 98%;
+  width: 75%;
   height: 50px;
-  background-color: rgba( 109, 7, 26, .95);
+  background-color: rgba(109, 7, 26, 0.95);
   z-index: 1;
   position: absolute;
   border-radius: 25px;
   display: flex;
   top: 6px;
-  right: 1%;
+  right: 15%;
   justify-content: flex-end;
   align-items: center;
+
+  @media (max-width: 1050px) {
+    width: 94%;
+    right: 3%;
+  }
 
   .heart {
     margin-right: 2%;
@@ -22,43 +27,40 @@ export const Nav = styled.div`
     }
   }
   .avatar {
-    
     img {
       border-radius: 50%;
       width: 27px;
       height: 27px;
     }
   }
-  
 `;
-
 
 export const Icons = styled.div`
   display: flex;
   margin-right: 10px;
   justify-content: space-between;
   width: 100px;
-  .signout{
+  .signout {
     img {
       border-radius: 50%;
       width: 27px;
       height: 27px;
-    } 
+    }
   }
 `;
 
 export const InButton = styled.button`
   width: 75px;
   height: 35px;
-  background-color: rgb(184,15,10);
+  background-color: rgb(184, 15, 10);
   color: white;
   border-radius: 20px;
   border: none;
   margin-right: 10px;
-  box-shadow: 0 4px 2px -2px rgba(15,15,15,.2);
+  box-shadow: 0 4px 2px -2px rgba(15, 15, 15, 0.2);
   &:hover {
-    background-color: rgba(184,15,10,.8);
-  } 
+    background-color: rgba(184, 15, 10, 0.8);
+  }
   &:focus {
     outline: 0;
   }
