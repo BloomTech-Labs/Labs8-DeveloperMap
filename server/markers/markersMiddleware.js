b75e53053d657222e6aaf06b2f62add848c9a1a8
lndubose <lndubose13@gmail.com>
2018-12-05 15:29:19 -0600

    Change Marker Seeker Middleware
    --Made profilePic more dynamic
    --Added if statement to check the profilePic and to change accordingly
    --Added val() to get the value from the database

--- server/markers/markersMiddleware.js
+++ server/markers/markersMiddleware.js
@@ -2,37 +2,56 @@ const firebase = require('../firebase/firebase.js');
 const rootRef = firebase.database().ref();
 
 const createMarkerObjectSeeker = async (req, res, next) => {
-  const { location, uid } = req.body;
-  let { firstName, lastName, jobTitle } = req.body;
+  const { uid } = req.body;
+  let { firstName, lastName, jobTitle, profilePicture, location } = req.body;
   if (!firstName) {
-    firstName = await rootRef.child(`seekers/${uid}/firstName`).once('value');
+    firstName = await rootRef
+      .child(`seekers/${uid}/firstName`)
+      .once('value')
+      .then(snapshot => snapshot.val());
   }
   if (!lastName) {
-    lastName = await rootRef.child(`seekers/${uid}/lastName`).once('value');
+    lastName = await rootRef
+      .child(`seekers/${uid}/lastName`)
+      .once('value')
+      .then(snapshot => snapshot.val());
   }
   if (!jobTitle) {
-    jobTitle = await rootRef.child(`seekers/${uid}/jobTitle`).once('value');
+    jobTitle = await rootRef
+      .child(`seekers/${uid}/jobTitle`)
+      .once('value')
+      .then(snapshot => snapshot.val());
   }
-  if (location) {
-    const markerData = {
-      geometry: {
-        // Convert Coordinates to Numbers
-        coordinates: location.coordinates.map(coord => Number(coord)),
-      },
-      properties: {
-        title: {
-          firstName,
-          lastName,
-        },
-        jobTitle,
-        profilePicture: '',
-        uid,
-        role: 'seeker',
-      },
-    };
-    req.body.markerData = markerData;
-    next();
+  if (!profilePicture) {
+    profilePicture = await rootRef
+      .child(`seekers/${uid}/profilePicture`)
+      .once('value')
+      .then(snapshot => snapshot.val());
   }
+  if (!location) {
+    location = await rootRef
+      .child(`seekers/${uid}/location`)
+      .once('value')
+      .then(snapshot => snapshot.val());
+  }
+
+  const markerData = {
+    geometry: {
+      // Convert Coordinates to Numbers
+      coordinates: location.coordinates.map(coord => Number(coord)),
+    },
+    properties: {
+      title: {
+        firstName,
+        lastName,
+      },
+      jobTitle,
+      profilePicture,
+      uid,
+      role: 'seeker',
+    },
+  };
+  req.body.markerData = markerData;
   next();
 };
 
