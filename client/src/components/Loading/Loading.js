aba0e34f4404b29cbf6721e229361dce7d238c86
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-12-10 11:39:03 -0700

    Create Loading Modal - Created Loading Modal Loading.js and containing folder. - Currently non-functional.

---
+++ client/src/components/Loading/Loading.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Loading extends Component {
+  render () {
+    return (
+      <div>I'm Loading Mom!</div>
+    )
+  }
+
+}
+
+export default Loading;
