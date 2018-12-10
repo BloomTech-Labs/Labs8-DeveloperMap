d3c7806e047f03e8cc994584eeaac5e9a90fc0bd
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-12-10 12:40:01 -0700

    Create SpinningGlobe Component and Styles - Spinning Globe Component Made, but empty, same with styles.

---
+++ client/src/components/Loading/SpinningGlobe.js
@@ -0,0 +1,10 @@
+import React from 'React';
+
+const SpinningGlobe = () => {
+  return (
+    <div className="globe-container">
+      <div className="globe-filter"/>
+      <div className="spinning-globe"/>
+    </div>
+  )
+}
