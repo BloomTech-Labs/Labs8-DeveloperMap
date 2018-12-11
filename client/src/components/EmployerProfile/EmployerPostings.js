210ddb36b5d64e634053b92a6fe04c2ed02608d7
ezra davis davis <ezra.rr.davis@gmail.com>
2018-12-11 15:58:33 -0800

    apart of last commit

--- client/src/components/EmployerProfile/EmployerPostings.js
+++ client/src/components/EmployerProfile/EmployerPostings.js
@@ -7,52 +7,48 @@ import axios from 'axios';
 
 import { Posts, PostContainer } from './EmployerStyles';
 
-const rootRef = firebase.database().ref();
-
 class EmployerPostings extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      favToggle: false,
-    };
+    this.state = {};
   }
 
-  favToggle = async (e, post) => {
-    e.preventDefault();
+  // favToggle = async (e, post) => {
+  //   e.preventDefault();
 
-    const user = this.props.user;
-    if (!user) {
-      this.props.history.push('/signin');
-    } else if (e.target.src === FavHeart) {
-      //Removes Favorited Post from current User if confirms
-      const favoritedList = [
-        ...this.state.favoritedList.filter(jobId => jobId !== e.target.id),
-      ];
-      this.setState({ favoritedList });
-      e.target.src = Heart;
-    } else {
-      //Adds Favorited Post to current User's list if confirms
-      const favoritedList = [...this.state.favoritedList];
-      favoritedList.push(e.target.id);
-      this.setState({ favoritedList });
-      e.target.src = FavHeart;
-    }
-  };
+  //   const user = this.props.user;
+  //   if (!user) {
+  //     this.props.history.push('/signin');
+  //   } else if (e.target.src === FavHeart) {
+  //     //Removes Favorited Post from current User if confirms
+  //     const favoritedList = [
+  //       ...this.state.favoritedList.filter(jobId => jobId !== e.target.id),
+  //     ];
+  //     this.setState({ favoritedList });
+  //     e.target.src = Heart;
+  //   } else {
+  //     //Adds Favorited Post to current User's list if confirms
+  //     const favoritedList = [...this.state.favoritedList];
+  //     favoritedList.push(e.target.id);
+  //     this.setState({ favoritedList });
+  //     e.target.src = FavHeart;
+  //   }
+  // };
 
   render() {
+    const { posts, favoritedList } = this.props;
     return (
       <PostContainer>
-        {this.state.posts ? (
+        {posts ? (
           <Posts>
-            {Object.values(this.state.posts).map((post, i) => (
+            {Object.values(posts).map((post, i) => (
               <JobPosting
                 key={i}
                 index={i}
-                favoritedList={this.state.favoritedList}
+                favoritedList={favoritedList}
                 {...this.props}
                 post={post}
-                heart={this.state.heart}
-                favToggle={this.favToggle}
+                favToggle={this.props.favToggle}
               />
             ))}
           </Posts>
