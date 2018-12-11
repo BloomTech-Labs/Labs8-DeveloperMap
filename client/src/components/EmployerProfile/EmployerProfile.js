5ca485c9de3eb62fbf648795645beb47a5df83c6
ezra davis davis <ezra.rr.davis@gmail.com>
2018-12-11 13:59:13 -0800

    New GETs hooked up to EmployerProfile

--- client/src/components/EmployerProfile/EmployerProfile.js
+++ client/src/components/EmployerProfile/EmployerProfile.js
@@ -4,55 +4,95 @@ import { ModalContainer, ModalMain } from '../../styles/ModalGlobalStyle';
 import profile from '../../images/avatar-icon.jpg';
 import { Info, Styling } from './EmployerStyles';
 import styled from 'styled-components';
+import axios from 'axios';
 import EmployerPostings from './EmployerPostings';
 
 class EmployerProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      companyName: '',
-      companyWebsite: '',
-      email: '',
-      location: {},
-      phoneNumber: '',
-      profilePicture: ''
+      loading: true,
     };
   }
 
   componentDidMount() {
-    const uid = this.props.match.params.employerId;
-    firebase
-      .database()
-      .ref()
-      .child(`companies/${uid}`)
-      .once('value')
-      .then(snapshot => {
-        this.setState(snapshot.val());
-      });
+    const employerId = this.props.match.params.employerId;
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        const { uid } = user;
+        if (this.state.posts) {
+          axios
+            .get(
+              `https://intense-stream-29923.herokuapp.com/api/database/favorites/keys/${uid}`
+            )
+            .then(response => {
+              const { favoritedList } = response.data;
+              const initialFavoritedList = favoritedList;
+              this.setState({
+                favoritedList,
+                initialFavoritedList,
+                loading: false;
+              });
+            });
+        } else {
+          axios
+            .get(
+              `https://intense-stream-29923.herokuapp.com/api/database/companies/allCompanyDataAndFavKeys/${employerId}/${uid}`
+            )
+            .then(response => {
+              const { posts, companyInfo, favoritedList } = response.data;
+              const initialFavoritedList = favoritedList;
+              this.setState({
+                posts,
+                companyInfo,
+                favoritedList,
+                initialFavoritedList,
+                loading: false,
+              });
+            });
+        }
+      } else {
+        axios.get(
+          `https://intense-stream-29923.herokuapp.com/api/database/companies/allCompanyData/${employerId}`
+        ).then(response => {
+          const {posts, companyInfo} = response.data
+          this.setState({
+            posts,
+            companyInfo,
+            loading: false,
+          })
+        })
+      }
+    });
   }
 
   render() {
+    const {profilePicture, companyName, location, companyWebsite, phoneNumber, email} = this.state.companyInfo
     return (
       <ModalContainer data-type="modal-container">
-        <ModalMain style={{ maxWidth: '750px', borderRadius:'20px' }} className="modal">
+        <ModalMain
+          style={{ maxWidth: '750px', borderRadius: '20px' }}
+          className="modal"
+        >
           <Styling>
             <Info>
-            {this.state.profilePicture ? (
-              <ProfilePic src={this.state.profilePicture} alt="profile" />
-            ) : (
-              <ProfilePic src={profile} alt="default" />
-            )}
-            <h2 className="com-name">{this.state.companyName}</h2>
-            <InfoContainer>
-              <h3>Location:</h3>
-              <h4>{this.state.location.city}</h4>
-              <h3>Website:</h3>
-              <h4>{this.state.companyWebsite}</h4>
-              <h3>Phone</h3>
-              <h4>{this.state.phoneNumber}</h4>
-              <h3>Email:</h3>
-              <h4>{this.state.email}</h4>
-            </InfoContainer>
+              {profilePicture ? (
+                <ProfilePic src={profilePicture} alt="profile" />
+              ) : (
+                <ProfilePic src={profile} alt="default" />
+              )}
+              <h2 className="com-name">{companyName}</h2>
+              <InfoContainer>
+                <h3>Location:</h3>
+                <h4>{location.city}</h4>
+                <h3>Website:</h3>
+                <h4>{companyWebsite}</h4>
+                <h3>Phone</h3>
+                <h4>{phoneNumber}</h4>
+                <h3>Email:</h3>
+                <h4>{email}</h4>
+              </InfoContainer>
             </Info>
             <EmployerPostings {...this.props} />
           </Styling>
@@ -70,7 +110,7 @@ export const ProfilePic = styled.img`
 
 const InfoContainer = styled.div`
   border: 0.7px solid rgba(220, 220, 220, 0.6);
-  border-radius: 4px; 
+  border-radius: 4px;
   width: 100%;
   padding: 2%;
 `;
