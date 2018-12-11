558ce5de861c1ba093c65a239af7a4e1f7e28077
ezra davis davis <ezra.rr.davis@gmail.com>
2018-12-11 15:58:20 -0800

    Favorite Toggling moved to parent componenet

--- client/src/components/EmployerProfile/EmployerProfile.js
+++ client/src/components/EmployerProfile/EmployerProfile.js
@@ -6,15 +6,42 @@ import { Info, Styling } from './EmployerStyles';
 import styled from 'styled-components';
 import axios from 'axios';
 import EmployerPostings from './EmployerPostings';
+import FavHeart from '../../images/favorites-icon.png';
+import Heart from '../../images/hollow-heart.png';
+import Loading from '../Loading/Loading';
+
+const rootRef = firebase.database().ref();
 
 class EmployerProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: true,
-      companyInfo: {}
+      companyInfo: {},
     };
   }
+
+  favToggle = e => {
+    e.preventDefault();
+
+    const user = this.props.user;
+    if (!user) {
+      this.props.history.push('/signin');
+    } else if (e.target.src === FavHeart) {
+      //Removes Favorited Post from current User if confirms
+      const favoritedList = [
+        ...this.state.favoritedList.filter(jobId => jobId !== e.target.id),
+      ];
+      this.setState({ favoritedList });
+      e.target.src = Heart;
+    } else {
+      //Adds Favorited Post to current User's list if confirms
+      const favoritedList = [...this.state.favoritedList];
+      favoritedList.push(e.target.id);
+      this.setState({ favoritedList });
+      e.target.src = FavHeart;
+    }
+  };
 
   componentDidMount() {
     const employerId = this.props.match.params.employerId;
@@ -33,7 +60,7 @@ class EmployerProfile extends React.Component {
               this.setState({
                 favoritedList,
                 initialFavoritedList,
-                loading: false;
+                loading: false,
               });
             });
         } else {
@@ -54,16 +81,18 @@ class EmployerProfile extends React.Component {
             });
         }
       } else {
-        axios.get(
-          `https://intense-stream-29923.herokuapp.com/api/database/companies/allCompanyData/${employerId}`
-        ).then(response => {
-          const {posts, companyInfo} = response.data
-          this.setState({
-            posts,
-            companyInfo,
-            loading: false,
-          })
-        })
+        axios
+          .get(
+            `https://intense-stream-29923.herokuapp.com/api/database/companies/allCompanyData/${employerId}`
+          )
+          .then(response => {
+            const { posts, companyInfo } = response.data;
+            this.setState({
+              posts,
+              companyInfo,
+              loading: false,
+            });
+          });
       }
     });
   }
@@ -91,37 +120,61 @@ class EmployerProfile extends React.Component {
   }
 
   render() {
-    const {companyInfo, posts, favoritedList, initialFavoritedList} = this.state
-    const {profilePicture, companyName, location, companyWebsite, phoneNumber, email} = this.state.companyInfo
+    const {
+      companyInfo,
+      posts,
+      favoritedList,
+      initialFavoritedList,
+    } = this.state;
+    const {
+      profilePicture,
+      companyName,
+      location,
+      companyWebsite,
+      phoneNumber,
+      email,
+    } = this.state.companyInfo;
     return (
-      <ModalContainer data-type="modal-container">
-        <ModalMain
-          style={{ maxWidth: '750px', borderRadius: '20px' }}
-          className="modal"
-        >
-          <Styling>
-            <Info>
-              {profilePicture ? (
-                <ProfilePic src={profilePicture} alt="profile" />
-              ) : (
-                <ProfilePic src={profile} alt="default" />
-              )}
-              <h2 className="com-name">{companyName}</h2>
-              <InfoContainer>
-                <h3>Location:</h3>
-                <h4>{location.city}</h4>
-                <h3>Website:</h3>
-                <h4>{companyWebsite}</h4>
-                <h3>Phone</h3>
-                <h4>{phoneNumber}</h4>
-                <h3>Email:</h3>
-                <h4>{email}</h4>
-              </InfoContainer>
-            </Info>
-            <EmployerPostings {...this.props} posts={posts} favoritedList={favoritedList} initialFavoritedList={initialFavoritedList} />
-          </Styling>
-        </ModalMain>
-      </ModalContainer>
+      <>
+        {this.state.loading ? (
+          <Loading />
+        ) : (
+          <ModalContainer data-type="modal-container">
+            <ModalMain
+              style={{ maxWidth: '750px', borderRadius: '20px' }}
+              className="modal"
+            >
+              <Styling>
+                <Info>
+                  {profilePicture ? (
+                    <ProfilePic src={profilePicture} alt="profile" />
+                  ) : (
+                    <ProfilePic src={profile} alt="default" />
+                  )}
+                  <h2 className="com-name">{companyName}</h2>
+                  <InfoContainer>
+                    <h3>Location:</h3>
+                    <h4>{location.city}</h4>
+                    <h3>Website:</h3>
+                    <h4>{companyWebsite}</h4>
+                    <h3>Phone</h3>
+                    <h4>{phoneNumber}</h4>
+                    <h3>Email:</h3>
+                    <h4>{email}</h4>
+                  </InfoContainer>
+                </Info>
+                <EmployerPostings
+                  {...this.props}
+                  favToggle={this.favToggle}
+                  posts={posts}
+                  favoritedList={favoritedList}
+                  initialFavoritedList={initialFavoritedList}
+                />
+              </Styling>
+            </ModalMain>
+          </ModalContainer>
+        )}
+      </>
     );
   }
 }
