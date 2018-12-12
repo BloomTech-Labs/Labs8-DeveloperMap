0c2f613bd7b88212740c54217c9e3f02e0a81643
ezra davis davis <ezra.rr.davis@gmail.com>
2018-12-11 16:07:34 -0800

    loading component attached to componenet

--- client/src/components/SeekerProfile/SeekerProfile.js
+++ client/src/components/SeekerProfile/SeekerProfile.js
@@ -21,6 +21,7 @@ import resumePic from '../../images/Icons/icons8-resume.png';
 import twitterPic from '../../images/Icons/icons8-twitter-filled.png';
 import paperPlane from '../../images/Icons/icons8-paper-plane-26.png';
 import phonePic from '../../images/Icons/icons8-phone-26.png';
+import Loading from '../Loading/Loading.js';
 
 class SeekerProfile extends React.Component {
   constructor(props) {
@@ -41,6 +42,7 @@ class SeekerProfile extends React.Component {
       remote: false,
       resume: '',
       twitter: '',
+      loading: true,
     };
   }
 
@@ -50,7 +52,7 @@ class SeekerProfile extends React.Component {
       .get(
         `https://intense-stream-29923.herokuapp.com/api/database/seekers/${uid}`
       )
-      .then(response => this.setState({ ...response.data }))
+      .then(response => this.setState({ ...response.data, loading: false }))
       .catch(err => {
         console.log('%cNot a user', 'color: red', err);
         this.props.history.push('/nouser');
@@ -63,123 +65,129 @@ class SeekerProfile extends React.Component {
 
   render() {
     return (
-      <ModalContainer data-type="modal-container">
-        <ProfileModalStyle className="modal">
-          <ProfileStyle>
-            <Icons>
-              <a
-                href={this.state.linkedIn}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <img
-                  className={this.state.linkedIn ? null : 'none'}
-                  src={linkedinPic}
-                  alt="LinkedIn"
-                />
-              </a>
-              <a
-                href={this.state.github}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <img
-                  className={this.state.github ? null : 'none'}
-                  src={githubPic}
-                  alt="Github"
-                />
-              </a>
-              <a
-                href={this.state.portfolio}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <img
-                  className={this.state.portfolio ? null : 'none'}
-                  src={computerPic}
-                  alt="Computer"
-                />
-              </a>
-              <a
-                href={this.state.twitter}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <img
-                  className={this.state.twitter ? null : 'none'}
-                  src={twitterPic}
-                  alt="Twitter"
-                />
-              </a>
-              <a
-                href={this.state.resume}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <img
-                  className={this.state.resume ? null : 'none'}
-                  src={resumePic}
-                  alt="Resume"
-                />
-              </a>
-            </Icons>
-            {this.state.profilePicture ? (
-              <ProfilePic image={this.state.profilePicture} alt="profile" />
-            ) : (
-              <ProfilePic image={profile} alt="default" />
-            )}
-            <MainContent>
-              <h1>{`${this.state.firstName} ${this.state.lastName}`}</h1>
-              <h3>
-                {this.state.location.city}, {this.state.location.state}
-              </h3>
-              {this.state.bio ? (
-                <div className="bio">
-                  <h4>Bio:</h4>
-                  <p>{this.state.bio}</p>
+      <>
+        {this.state.loading ? (
+          <Loading />
+        ) : (
+          <ModalContainer data-type="modal-container">
+            <ProfileModalStyle className="modal">
+              <ProfileStyle>
+                <Icons>
+                  <a
+                    href={this.state.linkedIn}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      className={this.state.linkedIn ? null : 'none'}
+                      src={linkedinPic}
+                      alt="LinkedIn"
+                    />
+                  </a>
+                  <a
+                    href={this.state.github}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      className={this.state.github ? null : 'none'}
+                      src={githubPic}
+                      alt="Github"
+                    />
+                  </a>
+                  <a
+                    href={this.state.portfolio}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      className={this.state.portfolio ? null : 'none'}
+                      src={computerPic}
+                      alt="Computer"
+                    />
+                  </a>
+                  <a
+                    href={this.state.twitter}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      className={this.state.twitter ? null : 'none'}
+                      src={twitterPic}
+                      alt="Twitter"
+                    />
+                  </a>
+                  <a
+                    href={this.state.resume}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      className={this.state.resume ? null : 'none'}
+                      src={resumePic}
+                      alt="Resume"
+                    />
+                  </a>
+                </Icons>
+                {this.state.profilePicture ? (
+                  <ProfilePic image={this.state.profilePicture} alt="profile" />
+                ) : (
+                  <ProfilePic image={profile} alt="default" />
+                )}
+                <MainContent>
+                  <h1>{`${this.state.firstName} ${this.state.lastName}`}</h1>
+                  <h3>
+                    {this.state.location.city}, {this.state.location.state}
+                  </h3>
+                  {this.state.bio ? (
+                    <div className="bio">
+                      <h4>Bio:</h4>
+                      <p>{this.state.bio}</p>
+                    </div>
+                  ) : (
+                    <span />
+                  )}
+                </MainContent>
+                <div className="options">
+                  <CheckLabel>
+                    <input
+                      type="checkbox"
+                      disabled={true}
+                      checked={this.state.remote}
+                    />
+                    <span className="slider round" />
+                    <span>Open to remote work</span>
+                  </CheckLabel>
+                  <CheckLabel>
+                    <input
+                      type="checkbox"
+                      disabled={true}
+                      checked={this.state.relocation}
+                    />
+                    <span>Open to relocation</span>
+                  </CheckLabel>
                 </div>
-              ) : (
-                <span />
-              )}
-            </MainContent>
-            <div className="options">
-              <CheckLabel>
-                <input
-                  type="checkbox"
-                  disabled={true}
-                  checked={this.state.remote}
-                />
-                <span className="slider round" />
-                <span>Open to remote work</span>
-              </CheckLabel>
-              <CheckLabel>
-                <input
-                  type="checkbox"
-                  disabled={true}
-                  checked={this.state.relocation}
-                />
-                <span>Open to relocation</span>
-              </CheckLabel>
-            </div>
-            {this.props.currentSignedInUser ? (
-              <Info>
-                <div className="info">
-                  <img src={paperPlane} alt="paper plane" />
-                  <p>{this.state.email}</p>
-                </div>
-                <div className="info">
-                  <img src={phonePic} alt="phone number" />
-                  <p>{this.state.phoneNumber}</p>
-                </div>
-              </Info>
-            ) : (
-              <SignEmployer>
-                <p onClick={this.signUpEmployer}>Sign Up Employer</p>
-              </SignEmployer>
-            )}
-          </ProfileStyle>
-        </ProfileModalStyle>
-      </ModalContainer>
+                {this.props.currentSignedInUser ? (
+                  <Info>
+                    <div className="info">
+                      <img src={paperPlane} alt="paper plane" />
+                      <p>{this.state.email}</p>
+                    </div>
+                    <div className="info">
+                      <img src={phonePic} alt="phone number" />
+                      <p>{this.state.phoneNumber}</p>
+                    </div>
+                  </Info>
+                ) : (
+                  <SignEmployer>
+                    <p onClick={this.signUpEmployer}>Sign Up Employer</p>
+                  </SignEmployer>
+                )}
+              </ProfileStyle>
+            </ProfileModalStyle>
+          </ModalContainer>
+        )}
+      </>
     );
   }
 }
