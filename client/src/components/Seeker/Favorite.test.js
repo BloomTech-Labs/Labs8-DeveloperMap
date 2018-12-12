372bed152eb7766333cdf82faed6ab3b4c51e9ec
Austinlp4 <apendergrast44@gmail.com>
2018-12-12 09:43:40 -0600

    set up favorites test suite

---
+++ client/src/components/Seeker/Favorite.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import SeekerFavorites from './SeekerFavorites';
+
+jest.mock('../../firebase/firebase', () => {
+    const firebasemock = require('firebase-mock');
+  
+    const mockdatabase = new firebasemock.MockFirebase();
+    const mockauth = new firebasemock.MockFirebase();
+    const mocksdk = new firebasemock.MockFirebaseSdk(
+      path => {
+        return path ? mockdatabase.child(path) : mockdatabase;
+      },
+      () => {
+        return mockauth;
+      }
+    );
+  
+    const firebase = mocksdk.initializeApp(); // can take a path arg to database url
+    // optional - expose the mock
+    global.firebase = firebase;
+  
+    // return the mock to match your export api
+    return firebase;
+  });
+
+
+describe(`favorite component`, () => {
+  it(`should render without error`, () => {
+    const props = {
+        favorite: {
+            companyName: 'jedi co',
+            date: '12/25/1992',
+            jobLink: 'indeed.com',
+            jobTitle: 'Dev',
+            location: {
+                city: 'Fayetteville',
+                state: 'Arkansas',
+                zip: '72701'
+            }
+        },
+        match: {
+            params: {
+                seekerId: 'uid1'
+            }
+        }
+    }
+    const tree = renderer.create(<SeekerFavorites {...props}/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
