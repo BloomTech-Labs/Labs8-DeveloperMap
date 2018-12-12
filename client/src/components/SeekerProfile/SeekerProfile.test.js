836babee09242ede7a1185e9615e128de6a34403
Austinlp4 <apendergrast44@gmail.com>
2018-12-12 09:47:08 -0600

    set up seeker profile test suite

---
+++ client/src/components/SeekerProfile/SeekerProfile.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import SeekerProfile from './SeekerProfile';
+
+jest.mock('../../firebase/firebase', () => {
+  const firebasemock = require('firebase-mock');
+
+  const mockdatabase = new firebasemock.MockFirebase();
+  const mockauth = new firebasemock.MockFirebase();
+  const mocksdk = new firebasemock.MockFirebaseSdk(
+    path => {
+      return path ? mockdatabase.child(path) : mockdatabase;
+    },
+    () => {
+      return mockauth;
+    }
+  );
+
+  const firebase = mocksdk.initializeApp(); // can take a path arg to database url
+  // optional - expose the mock
+  global.firebase = firebase;
+
+  // return the mock to match your export api
+  return firebase;
+});
+
+describe(`seeker profile component`, () => {
+  it(`should render without error`, () => {
+    const props = {
+      match: {
+        params: {
+          seekerId: 'uid1',
+        },
+      },
+    };
+
+    const tree = renderer.create(<SeekerProfile {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
